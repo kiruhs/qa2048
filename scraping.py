102e8bcfe75a86a75293c16721997e815871c0ea
kiruhs <87802917+kiruhs@users.noreply.github.com>
2026-02-19 09:16:39 +0200

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,17 +1,74 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-url = "https://google.com"
+from requests.exceptions import JSONDecodeError
+import re
+from pprint import pprint
 
-response = requests.get(url)
+# url = "https://google.com"
+
+# response = requests.get(url)
 # j = response.json()
 # print(j['origin'])
 
 # print(type(j))
 # print(json.dumps(j, indent=4, sort_keys=True)) # print json in pretty mode
 
-soup = BeautifulSoup(response.content, 'lxml')
-print(soup)
+# soup = BeautifulSoup(response.content, 'lxml')
+# print(soup)
 
-tag = soup.button
+# tag = soup.button
 # print(tag.attrs)
+
+response = requests.get('https://github.com')
+
+# try:
+#     print(response.json())
+# except JSONDecodeError:
+#     print("cannot extract JSON from requested server")
+# finally:
+#     text = response.text
+# print(text)
+
+soup = BeautifulSoup(response.content, 'lxml')
+# print(soup)
+
+# tag = soup.button
+# print(tag.attrs)
+# print(tag.attrs['class'][2])
+
+# buttons = soup.find_all('button')
+# print(len(buttons))
+# for b in buttons:
+#     print(b.attrs)
+
+# print(buttons[-1])
+
+# print(soup.a.get('href'))
+# for link in range(len(soup.find_all('a'))):
+#     if re.search('^http', soup.find_all('a')[link].get('href')):
+#         print(link+1, ': ', soup.find_all('a')[link].get('href'))
+
+
+# result = re.search('ell', "Helo, world, llo")
+# print(result)
+
+# h = soup.find_all('a', href=re.compile('http'))
+# print(h)
+
+# print(soup.find_all('title', string='American Airlines'))
+
+# for tag in soup.find_all():
+#     print(tag.name)
+
+tag_list = [tag.name for tag in soup.find_all()]
+# print(len(set(tag_list)))
+tag_dict = {}
+for i in tag_list:
+    if i in tag_dict:
+        tag_dict[i] += 1
+    else:
+        tag_dict[i] = 1
+
+# print(tag_dict)
+pprint(dict(sorted(tag_dict.items(), key=lambda tg: tg[1], reverse=True)), sort_dicts=False)
--- youtube.py
+++ youtube.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome()
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 driver.maximize_window()
 driver.get("https://www.youtube.com/watch?v=voO12-fh-eU")
 #driver.execute_script("window.scrollBy(0, 500)")
@@ -18,16 +22,20 @@ while True:
     except Exception:
         pass
 
-driver.execute_script("window.open('https://google.com', '_blank')")
-sleep(2)
-driver.switch_to.window(driver.window_handles[1])
-search = driver.find_element(By.ID, 'APjFqb')
-search.click()
-search.send_keys("selenium")
-search.send_keys(Keys.ENTER)
-sleep(3)
-driver.execute_script("window.open('https://cnn.com', '_blank')")
-sleep(3)
-for window in driver.window_handles:
-    driver.switch_to.window(window)
-    sleep(2)
+
+
+
+
+# driver.execute_script("window.open('https://google.com', '_blank')")
+# sleep(2)
+# driver.switch_to.window(driver.window_handles[1])
+# search = driver.find_element(By.ID, 'APjFqb')
+# search.click()
+# search.send_keys("selenium")
+# search.send_keys(Keys.ENTER)
+# sleep(3)
+# driver.execute_script("window.open('https://cnn.com', '_blank')")
+# sleep(3)
+# for window in driver.window_handles:
+#     driver.switch_to.window(window)
+#     sleep(2)
